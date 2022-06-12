85768e86f8a9374d864b69094db75f7fa6e54f07
pattybruh <patrick.h.li@gmail.com>
2022-06-12 00:47:32 -0700

    coords shift map

--- static/js/index.js
+++ static/js/index.js
@@ -6,8 +6,20 @@ var map = new ol.Map({
     })
   ],
   view: new ol.View({
-    //LONGITUDE FIRST IDK WHY IT DOES THAT (-98.57, 39.82)
+    //LONGITUDE FIRST IDK WHY IT DOES THAT (39.82, -98.57)
     center: ol.proj.fromLonLat([-98.57, 39.82]),
     zoom: 4
   })
 });
+var c;
+function CenterMap(long, lat){
+  map.getView().setCenter(ol.proj.transform([long, lat], 'EPSG:4326', 'EPSG:3857'));
+  map.getView().setZoom(4);
+}
+function process(){
+  c = document.getElementsByName("coordinates")[0].value;
+  String(c);
+  var cArr = c.split(',');
+  CenterMap(parseInt(cArr[1]), parseInt(cArr[0]));
+  return false;
+}
