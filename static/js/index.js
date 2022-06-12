ac6c867acf0cef4ea36032d8480c6f102a394c67
pattybruh <patrick.h.li@gmail.com>
2022-06-12 01:35:07 -0700

    for richards bitching ass that keeps complaining about my coordinates

--- static/js/index.js
+++ static/js/index.js
@@ -15,24 +15,30 @@ var c;
 const dir = ["S", "W"]
 function CenterMap(long, lat){
   map.getView().setCenter(ol.proj.fromLonLat([long, lat]));
-  map.getView().setZoom(4);
+  map.getView().setZoom(16);
 }
 function process(){
   c = document.getElementsByName("coordinates")[0].value;
   var cArr = [];
   String(c);
   var count = 0;
-  for (var i=0; i<c.length; i++){
-    if(c[i] == "°" && count == 0){
-      cArr.push(c.slice(0, i));
-      count=i;
-    }
-    else if(c[i] == "°"){
-      cArr.push(c.slice(count+4, i));
-    }
-    else if(dir.includes(c[i])){
-      var neg = "-"
-      cArr[cArr.length-1] = neg.concat(cArr[cArr.length-1]);
+  var isValid = /^[0-9,.\s\-]*$/.test(c);
+  if(isValid){
+    cArr = c.split(',');
+  }
+  else{
+    for (var i=0; i<c.length; i++){
+      if(c[i] == "°" && count == 0){
+        cArr.push(c.slice(0, i));
+        count=i;
+      }
+      else if(c[i] == "°"){
+        cArr.push(c.slice(count+4, i));
+      }
+      else if(dir.includes(c[i])){
+        var neg = "-"
+        cArr[cArr.length-1] = neg.concat(cArr[cArr.length-1]);
+      }
     }
   }
   //var cArr = c.split(',');
