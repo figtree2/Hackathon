78ee77c3a45c863d90307a75d158b9a10abeeb99
pattybruh <patrick.h.li@gmail.com>
2022-06-12 00:55:37 -0700

    wrong cmamnd oops

--- static/js/index.js
+++ static/js/index.js
@@ -13,7 +13,7 @@ var map = new ol.Map({
 });
 var c;
 function CenterMap(long, lat){
-  map.getView().setCenter(ol.proj.transform([long, lat], 'EPSG:4326', 'EPSG:3857'));
+  map.getView().setCenter(ol.proj.fromLonLat([long, lat]));
   map.getView().setZoom(4);
 }
 function process(){
