e3108c3d72c46e44362d3b818b17ab1e915ed334
figtree2 <66570700+figtree2@users.noreply.github.com>
2022-06-11 18:03:52 -0700

    mongo conection

--- main.py
+++ main.py
@@ -91,5 +91,20 @@ def logout():
     session.clear()
     return redirect("/")
 
+def get_database():
+    from pymongo import MongoClient
+    import pymongo
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = "mongodb+srv://figtree:1234@cluster0.j0ptd.mongodb.net/?retryWrites=true&w=majority"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    from pymongo import MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client['Cluster0']
+
 if __name__ == '__main__':
     app.run(debug=True)
+    dbname = get_database()
